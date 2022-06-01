a2fc173ac81a348cd324769744a3416377afdf6b
Arturo rodriguez <arturo.itiguala@gmail.com>
2022-06-01 14:43:14 -0500

    se agrega tailwind css y alpine js

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,2 +1,4 @@
+  <script src="./public/js/main.js"></script>
+  <script src="./public/js/nav.js"></script>
 </body>
 </html>
--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -5,8 +5,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= SITENAME ?></title>
-  <link rel="stylesheet" href="./public/css/tailwind.css" />
+  <!-- <link rel="stylesheet" href="./public/css/tailwind.css" /> -->
+  <script defer src="./public/js/alpine.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>  
+  <script src="./public/js/tailwind.js"></script>
+
   <link rel="stylesheet" href="./public/css/boxicons-2.1.2/css/boxicons.css" />
   <link rel="stylesheet" href="./public/css/main.css" />
+
 </head>
-<body class="bg-red-700">
+<body class="bg-main font-biz">
+  <?php require(APPROOT . '/views/partials/nav.php'); ?>
